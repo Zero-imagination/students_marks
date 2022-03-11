dd75c9e6da1a17caea40a3372bd5293072a1d1ec
Zero-imagination <disagreed7@gmail.com>
2022-03-12 01:45:05 +0300

    added a table for average grades for each subject and a table for all grades of the subject, also added buttons for editing data in these tables

--- src/main/java/gui/boxes/BoxStudent.java
+++ src/main/java/gui/boxes/BoxStudent.java
@@ -1,70 +1,122 @@
 package gui.boxes;
 
+import gui.ButtonControlMark;
 import gui.ButtonControlStudent;
+import gui.ComboBoxSubjects;
 import gui.TypeButton;
-import gui.tables.TableModelStudents;
-import gui.tables.TableStudents;
+import gui.tables.TableAvgMarks;
+import gui.tables.model.TableModelStudents;
+import gui.tables.TableSubjectMarks;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class BoxStudent {
-    public static Box createBox(JFrame mainFrame, JTable tableMarks){
+    private static final ArrayList<JTextField> infoTextFields = new ArrayList<>();
+    private static final ArrayList<JComponent> infoMarkFields = new ArrayList<>();
+    public static ArrayList<JComponent> getInfoMarkFields(){return infoMarkFields;}
+    public static ArrayList<JTextField> getInfoTextFields() {
+        return infoTextFields;
+    }
+
+    public static Box create(){
+        Font primaryFont = new Font("TimesRoman", Font.PLAIN, 14);
         Box boxStudent = new Box(BoxLayout.X_AXIS);
         Box boxStudentInfo = new Box(BoxLayout.PAGE_AXIS);
         JLabel labelStudentTitle = new JLabel("Студент");
-        JButton btnUpdateStudent = ButtonControlStudent.createButton(mainFrame, TypeButton.UPDATE);
+        JButton btnUpdateStudent = ButtonControlStudent.createButton(TypeButton.UPDATE);
         btnUpdateStudent.setText("Редактировать");
-        JButton btnCreateStudent = ButtonControlStudent.createButton(mainFrame, TypeButton.CREATE);
+        JButton btnCreateStudent = ButtonControlStudent.createButton(TypeButton.CREATE);
         btnCreateStudent.setText("Создать");
-        JButton btnDeleteStudent = ButtonControlStudent.createButton(mainFrame, TypeButton.DELETE);
+        JButton btnDeleteStudent = ButtonControlStudent.createButton(TypeButton.DELETE);
         btnDeleteStudent.setText("Удалить");
 
-        JPanel grid = new JPanel();
-        grid.setLayout(new GridLayout(13,1,10,0));
-        grid.setBorder((BorderFactory.createEmptyBorder(5, 10, 5, 10)));
-        grid.setMaximumSize(new Dimension(200,600));
+        JPanel gridPanelFields = new JPanel();
+        gridPanelFields.setLayout(new GridLayout(13,1,10,0));
+        gridPanelFields.setBorder((BorderFactory.createEmptyBorder(5, 10, 5, 10)));
+        gridPanelFields.setMaximumSize(new Dimension(200,600));
 
-        grid.add(labelStudentTitle);
+        gridPanelFields.add(labelStudentTitle);
 
         ArrayList<JLabel> infoLabels = new ArrayList<>();
         for (int i = 0; i<6; i++){
             infoLabels.add(new JLabel((String) TableModelStudents.getColumnsHeader()[i]));
-            infoLabels.get(i).setMaximumSize(new Dimension(170, 20));
+            //infoLabels.get(i).setFont(primaryFont);
+            infoLabels.get(i).setMaximumSize(new Dimension(200, 20));
+
             //infoLabels.get(i).setBorder(BorderFactory.createEmptyBorder(0, -50, 0, 0));
         }
-        ArrayList<JTextField> infoTextFields = new ArrayList<>();
         for (int i = 0; i<6; i++){
             infoTextFields.add(new JTextField());
+            infoTextFields.get(i).setFont(primaryFont);
             infoTextFields.get(i).setMaximumSize(new Dimension(170, 20));
         }
         infoTextFields.get(0).setEditable(false);
         for (int i = 0; i<6; i++){
-            grid.add(infoLabels.get(i));
-            grid.add(infoTextFields.get(i));
+            gridPanelFields.add(infoLabels.get(i));
+            gridPanelFields.add(infoTextFields.get(i));
         }
 
-        TableStudents.setInfoTextFields(infoTextFields);
-        ButtonControlStudent.setInfoTextFields(infoTextFields);
+        Box boxControlStudent = new Box(BoxLayout.LINE_AXIS);
+        boxControlStudent.setMaximumSize(new Dimension(300,70));
+        boxControlStudent.add(btnUpdateStudent);
+        boxControlStudent.add(btnCreateStudent);
+        boxControlStudent.add(btnDeleteStudent);
 
-        Box buttonControlStudent = new Box(BoxLayout.LINE_AXIS);
-        buttonControlStudent.setMaximumSize(new Dimension(300,70));
-        buttonControlStudent.add(btnUpdateStudent);
-        buttonControlStudent.add(btnCreateStudent);
-        buttonControlStudent.add(btnDeleteStudent);
+        boxStudentInfo.add(gridPanelFields);
+        boxStudentInfo.add(boxControlStudent);
 
-        boxStudentInfo.add(grid);
-        boxStudentInfo.add(buttonControlStudent);
+        JPanel panelGridControlMark = new JPanel();
+        panelGridControlMark.setLayout(new GridLayout(4,2));
+        JLabel labelMark = new JLabel("Оценка");
+        JLabel labelSubject = new JLabel("Предмет");
+        infoMarkFields.add(ComboBoxSubjects.create());
+        infoMarkFields.add(new JTextField());
 
-        JScrollPane scrollTableMarks = new JScrollPane(tableMarks);
+
+        JButton buttonCreateMark = ButtonControlMark.create(TypeButton.CREATE);
+        buttonCreateMark.setText("Добавить");
+        JButton buttonUpdateMark = ButtonControlMark.create(TypeButton.UPDATE);
+        buttonUpdateMark.setText("Редактировать");
+        JButton buttonDeleteMark = ButtonControlMark.create(TypeButton.DELETE);
+        buttonDeleteMark.setText("Удалить");
+
+
+        panelGridControlMark.add(labelSubject);
+        panelGridControlMark.add(infoMarkFields.get(0));
+        panelGridControlMark.add(labelMark);
+        panelGridControlMark.add(infoMarkFields.get(1));
+        panelGridControlMark.add(buttonCreateMark);
+        panelGridControlMark.add(buttonUpdateMark);
+        panelGridControlMark.add(buttonDeleteMark);
+
+
+        JScrollPane scrollTableAvgMarks = new JScrollPane(TableAvgMarks.create());
+        JScrollPane scrollTableSubjectMarks = new JScrollPane(TableSubjectMarks.create());
+
+
+        Box boxTableMarks = new Box(BoxLayout.Y_AXIS);
+        Box boxTables = new Box(BoxLayout.X_AXIS);
+        boxTables.add(scrollTableAvgMarks);
+        boxTables.add(scrollTableSubjectMarks);
+
+        boxTableMarks.add(boxTables);
+        boxTableMarks.add(panelGridControlMark);
+
 
         boxStudent.add(boxStudentInfo);
-        boxStudent.add(scrollTableMarks);
+        boxStudent.add(boxTableMarks);
 
+        gridPanelFields.setMaximumSize(new Dimension(200, 400));
         labelStudentTitle.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 10));
         boxStudentInfo.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-        scrollTableMarks.setMaximumSize(new Dimension(300,400));
+        panelGridControlMark.setMaximumSize(new Dimension(300,400));
+        panelGridControlMark.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+        scrollTableSubjectMarks.setMaximumSize(new Dimension(150,400));
+        scrollTableAvgMarks.setMaximumSize(new Dimension(300,400));
+        scrollTableAvgMarks.setMinimumSize(new Dimension(200,400));
+        boxTables.setMaximumSize(new Dimension(350,400));
         return boxStudent;
     }
 }
