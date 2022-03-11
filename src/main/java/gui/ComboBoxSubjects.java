f8357473409978c626924e8e97f225208f24291e
Zero-imagination <disagreed7@gmail.com>
2022-03-12 01:49:27 +0300

    combobox for selecting an subject

---
+++ src/main/java/gui/ComboBoxSubjects.java
@@ -0,0 +1,23 @@
+package gui;
+
+
+import models.Subject;
+import services.SubjectServiceImpl;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ComboBoxSubjects {
+    public static JComboBox<Object> create(){
+        SubjectServiceImpl subjectService = new SubjectServiceImpl();
+        List<Subject> subjects = subjectService.readListSubjects();
+
+        ArrayList<String> items = new ArrayList<>();
+        for (Subject subject : subjects){
+            items.add(subject.getSubjectName());
+        }
+        Collections.sort(items);
+        return new JComboBox<>(items.toArray());
+    }
+}
