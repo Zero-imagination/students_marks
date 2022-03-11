f4153bdf157c4858579c680ce12673d073a93467
Zero-imagination <disagreed7@gmail.com>
2022-03-12 01:50:42 +0300

    renamed variables for better perception

--- src/main/java/gui/Gui.java
+++ src/main/java/gui/Gui.java
@@ -8,27 +8,31 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Gui {
+    private static final JFrame mainFrame = new JFrame();
+
+    public static JFrame getMainFrame() {
+        return mainFrame;
+    }
+
     public static void createGui()
     {
-        JFrame frame = new JFrame("Students and marks");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JTable tableMarks = new JTable();
-        JTable tableStudents = TableStudents.createTable(tableMarks);
+        mainFrame.setTitle("Students and marks");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Box contents = new Box(BoxLayout.Y_AXIS);
 
-        JScrollPane scrollTableStudents = new JScrollPane(tableStudents);
-        Box boxStudent = BoxStudent.createBox(frame, tableMarks);
-        Box boxSearchBar = BoxSearchBar.createBox(tableStudents);
+        JScrollPane scrollTableStudents = new JScrollPane(TableStudents.create());
+        Box boxStudent = BoxStudent.create();
+        Box boxSearchBar = BoxSearchBar.create();
+
         scrollTableStudents.setMaximumSize(new Dimension(4000, 300));
 
         contents.add(boxSearchBar);
         contents.add(scrollTableStudents);
         contents.add(boxStudent);
 
-        frame.getContentPane().add(contents);
-        frame.setSize(700,700);
-        frame.setVisible(true);
+        mainFrame.getContentPane().add(contents);
+        mainFrame.setSize(700,700);
+        mainFrame.setVisible(true);
     }
 }
