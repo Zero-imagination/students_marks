0193bbed8b9108e11a6219d9b4385c570f25bcdb
Zero-imagination <disagreed7@gmail.com>
2022-03-12 02:02:19 +0300

    made filling in with data into a separate function

--- src/main/java/gui/tables/model/TableModelStudents.java
+++ src/main/java/gui/tables/model/TableModelStudents.java
@@ -1,4 +1,4 @@
-package gui.tables;
+package gui.tables.model;
 
 import models.Student;
 import services.StudentServiceImpl;
@@ -15,7 +15,7 @@ public class TableModelStudents extends JFrame {
         return columnsHeader;
     }
 
-    public static TableModel createTableModelStudents(String searchQuery){
+    public static TableModel create(String searchQuery){
         DefaultTableModel tableModel = new DefaultTableModel(){
             @Override
             public boolean isCellEditable(int i, int i1) {
@@ -27,16 +27,19 @@ public class TableModelStudents extends JFrame {
         StudentServiceImpl studentService = new StudentServiceImpl();
 
         List<Student> studentList = studentService.readListStudents(searchQuery);
+        //сортировка
+        //studentList.sort(Comparator.comparing(Student::getSurname));
 
         for (Student student : studentList){
             System.out.println(student);
-            Object[] arrayStudent = new String[]{
-                    String.valueOf(student.getId()),
-                    student.getSurname(), student.getName(), student.getPatronymic(),
-                    String.valueOf(student.getDateStartLearning()), String.valueOf(student.getDateEndLearning())};
-
-            tableModel.addRow(arrayStudent);            
+            tableModel.addRow(getDataStudent(student));
         }
         return tableModel;
+    }
+    public static Object[] getDataStudent (Student student){
+        return new String[]{
+                String.valueOf(student.getId()),
+                student.getSurname(), student.getName(), student.getPatronymic(),
+                String.valueOf(student.getDateStartLearning()), String.valueOf(student.getDateEndLearning())};
     }
 }
