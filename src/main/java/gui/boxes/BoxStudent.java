29331d60f8228c1f1d7c2375350ae4c935aa97b4
Zero-imagination <disagreed7@gmail.com>
2022-03-13 21:56:58 +0300

    added a caption to the table with ratings on top for easy viewing

--- src/main/java/gui/boxes/BoxStudent.java
+++ src/main/java/gui/boxes/BoxStudent.java
@@ -13,6 +13,12 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class BoxStudent {
+    private static JLabel selectedSubjectName = new JLabel("Название предмета");
+
+    public static JLabel getSelectedSubjectName() {
+        return selectedSubjectName;
+    }
+
     private static final ArrayList<JTextField> infoTextFields = new ArrayList<>();
     private static final ArrayList<JComponent> infoMarkFields = new ArrayList<>();
     public static ArrayList<JComponent> getInfoMarkFields(){return infoMarkFields;}
@@ -68,29 +74,33 @@ public class BoxStudent {
         boxStudentInfo.add(boxControlStudent);
 
         JPanel panelGridControlMark = new JPanel();
-        panelGridControlMark.setLayout(new GridLayout(4,2));
+        panelGridControlMark.setLayout(new GridLayout(2,3));
         JLabel labelMark = new JLabel("Оценка");
-        JLabel labelSubject = new JLabel("Предмет");
         infoMarkFields.add(ComboBoxSubjects.create());
         infoMarkFields.add(new JTextField());
 
 
         JButton buttonCreateMark = ButtonControlMark.create(TypeButton.CREATE);
+        ButtonControlMark.setButtonCreate(buttonCreateMark);
         buttonCreateMark.setText("Добавить");
         JButton buttonUpdateMark = ButtonControlMark.create(TypeButton.UPDATE);
+        ButtonControlMark.setButtonUpdate(buttonUpdateMark);
         buttonUpdateMark.setText("Редактировать");
         JButton buttonDeleteMark = ButtonControlMark.create(TypeButton.DELETE);
+        ButtonControlMark.setButtonDelete(buttonDeleteMark);
         buttonDeleteMark.setText("Удалить");
+        //JButton buttonAddSubject = new JButton();
+        //buttonAddSubject.setText("Добавить предмет");
 
+        //panelGridControlMark.add(labelSubject);
 
-        panelGridControlMark.add(labelSubject);
-        panelGridControlMark.add(infoMarkFields.get(0));
         panelGridControlMark.add(labelMark);
         panelGridControlMark.add(infoMarkFields.get(1));
+        panelGridControlMark.add(infoMarkFields.get(0));
         panelGridControlMark.add(buttonCreateMark);
         panelGridControlMark.add(buttonUpdateMark);
         panelGridControlMark.add(buttonDeleteMark);
-
+        //panelGridControlMark.add(buttonAddSubject);
 
         JScrollPane scrollTableAvgMarks = new JScrollPane(TableAvgMarks.create());
         JScrollPane scrollTableSubjectMarks = new JScrollPane(TableSubjectMarks.create());
@@ -98,9 +108,17 @@ public class BoxStudent {
 
         Box boxTableMarks = new Box(BoxLayout.Y_AXIS);
         Box boxTables = new Box(BoxLayout.X_AXIS);
+
+        JLabel subjectName = selectedSubjectName;
+
+        JPanel panelHeaderSubjectName = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        panelHeaderSubjectName.setMaximumSize(new Dimension(320,30));
+        panelHeaderSubjectName.add(subjectName);
+
         boxTables.add(scrollTableAvgMarks);
         boxTables.add(scrollTableSubjectMarks);
 
+        boxTableMarks.add(panelHeaderSubjectName);
         boxTableMarks.add(boxTables);
         boxTableMarks.add(panelGridControlMark);
 
@@ -111,12 +129,12 @@ public class BoxStudent {
         gridPanelFields.setMaximumSize(new Dimension(200, 400));
         labelStudentTitle.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 10));
         boxStudentInfo.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-        panelGridControlMark.setMaximumSize(new Dimension(300,400));
+        panelGridControlMark.setMaximumSize(new Dimension(350,400));
         panelGridControlMark.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
         scrollTableSubjectMarks.setMaximumSize(new Dimension(150,400));
         scrollTableAvgMarks.setMaximumSize(new Dimension(300,400));
         scrollTableAvgMarks.setMinimumSize(new Dimension(200,400));
-        boxTables.setMaximumSize(new Dimension(350,400));
+        boxTables.setMaximumSize(new Dimension(400,400));
         return boxStudent;
     }
 }
