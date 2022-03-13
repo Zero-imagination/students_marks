87c0d42a06f55b3521b2c9fa24e8dc9f1b4fede1
Zero-imagination <disagreed7@gmail.com>
2022-03-13 21:44:35 +0300

    redesigned the processing of the selected subject, added coloring of cells depending on the score

--- src/main/java/gui/tables/TableSubjectMarks.java
+++ src/main/java/gui/tables/TableSubjectMarks.java
@@ -3,46 +3,64 @@ package gui.tables;
 import gui.ButtonControlMark;
 import gui.boxes.BoxStudent;
 import models.Mark;
-
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import java.util.Set;
+import java.awt.*;
+import java.util.List;
 
 
+//сделать сортировку оценок по умолчанию
 public class TableSubjectMarks {
     private static JTable table;
-    private static Mark selectedSubjectMark;
-    //private static int selectedRow;
-
-    public static Mark getSelectedSubjectMark() {
-
-        return selectedSubjectMark;
+    private static Mark selectedMark;
+    public static Mark getSelectedMark() {
+        return selectedMark;
     }
-
     public static JTable getTable() {
         return table;
     }
 
     public static JTable create(){
-        JTable tableSubjectMarks = new JTable();
+        JTable tableSubjectMarks = new JTable(){
+            @Override
+            public Class<?> getColumnClass(int column) {
+                if(convertColumnIndexToModel(column)==0) return Object.class;
+                return super.getColumnClass(column);
+            }
+        };
+        Color green = new Color(153,204,153);
+        Color lightGreen = new Color(231,255,206);
+        Color lightOrange = new Color(242,219,180);
+        Color lightRed = new Color(255,213,206);
+        tableSubjectMarks.setDefaultRenderer(Object.class, new DefaultTableCellRenderer(){
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component c = super.getTableCellRendererComponent(table,value,isSelected,hasFocus,row,column);
 
+                if (Double.parseDouble(value.toString()) >4.0) c.setBackground(green);
+                else if (Double.parseDouble(value.toString()) >3.0) c.setBackground(lightGreen);
+                else if (Double.parseDouble(value.toString()) >2.0) c.setBackground(lightOrange);
+                else c.setBackground(lightRed);
+                return c;
+            }
+        });
+        tableSubjectMarks.setAutoCreateRowSorter(true);
         tableSubjectMarks.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 int row = tableSubjectMarks.rowAtPoint(evt.getPoint());
                 if (row >= 0) {
-                    Set<Mark> marks = TableStudents.getSelectedStudent().getAllMarks();
-                    for (Mark mark : marks){
-                        if (mark.getCurrentMark()==Double.parseDouble(tableSubjectMarks.getValueAt(row,0).toString())){
-                            TableSubjectMarks.selectedSubjectMark=mark;
-                            break;
-                        }
+                    double currentMark = Double.parseDouble(String.valueOf(tableSubjectMarks.getValueAt(row,0)));
+                    String subjectName=BoxStudent.getSelectedSubjectName().getText();
+                    List<Mark> subjectMarks = TableStudents.getSelectedStudent().getAverageMarkSubject(subjectName).getSubjectMark();
+                    for (Mark mark : subjectMarks){
+                        if(mark.getCurrentMark()==currentMark)
+                            selectedMark =mark;
                     }
-                    //selectedRow =row;
-
-                    ButtonControlMark.enableButtonDeleteUpdate(true);
-
+                    ButtonControlMark.enableButtonUpdateDeleteMark(true);
                     ((JTextField)BoxStudent.getInfoMarkFields().get(1)).setText(tableSubjectMarks.getValueAt(row,0).toString());
+                    (BoxStudent.getInfoMarkFields().get(0)).setEnabled(false);
                 }
             }
         });
@@ -51,19 +69,9 @@ public class TableSubjectMarks {
     }
 
     public static void refresh(){
-        ButtonControlMark.enableButtonDeleteUpdate(false);
+        ButtonControlMark.enableButtonUpdateDeleteMark(false);
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         model.setRowCount(0);
     }
-    /*public static void deleteRow(){
-        ((DefaultTableModel) table.getModel()).removeRow(selectedRow);
-    }
-    public static void addRow(Object[] rowData)
-    {
-        ((DefaultTableModel) table.getModel()).addRow(rowData);
-    }
-    public static void updateRow(Object[] rowData){
-        ((DefaultTableModel) table.getModel()).removeRow(selectedRow);
-        ((DefaultTableModel) table.getModel()).insertRow(selectedRow, rowData);
-    }*/
+
 }
