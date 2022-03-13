1f5de9203e4eaf581f80b2f911194a6f1a2b201e
Zero-imagination <disagreed7@gmail.com>
2022-03-13 21:41:22 +0300

    added disabling grade editing buttons when switching between students

--- src/main/java/gui/tables/TableStudents.java
+++ src/main/java/gui/tables/TableStudents.java
@@ -1,27 +1,24 @@
 package gui.tables;
 
+import gui.ButtonControlMark;
 import gui.boxes.BoxStudent;
 import gui.tables.model.TableModelAvgMarks;
 import models.Student;
 import services.StudentServiceImpl;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class TableStudents {
+    private static Student selectedStudent;
     private static int selectedRow;
     private static JTable table;
 
     public static Student getSelectedStudent() {
         return selectedStudent;
     }
-
-    public static void setSelectedStudent(Student selectedStudent) {
-        TableStudents.selectedStudent = selectedStudent;
+    public static void setSelectedStudent(Student student) {
+        selectedStudent = student;
     }
-
-    private static Student selectedStudent;
-
     public static JTable getTable() {
         return table;
     }
@@ -41,6 +38,8 @@ public class TableStudents {
                         BoxStudent.getInfoTextFields().get(i).setText(tableStudents.getValueAt(row,i).toString());
                     }
                 }
+                ButtonControlMark.enableButtonCreateMark(true);
+                (BoxStudent.getInfoMarkFields().get(0)).setEnabled(true);
                 TableSubjectMarks.refresh();
             }
         });
@@ -58,4 +57,5 @@ public class TableStudents {
         ((DefaultTableModel) table.getModel()).removeRow(selectedRow);
         ((DefaultTableModel) table.getModel()).insertRow(selectedRow, rowData);
     }
+
 }
