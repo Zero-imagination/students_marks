a2e6d7e903331a7d1de8b48c86d8fb98ac229ab5
Zero-imagination <disagreed7@gmail.com>
2022-03-19 13:49:25 +0300

    cleaned up unnecessary comments

--- src/main/java/evaluation/model/AverageMarkSubject.java
+++ src/main/java/evaluation/model/AverageMarkSubject.java
@@ -1,11 +1,11 @@
-package models;
+package evaluation.model;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class AverageMarkSubject {
+public class AverageMarkSubject implements Comparable<AverageMarkSubject>{
     private Subject subject;
-    private List<Mark> subjectMark = new LinkedList<>();
+    private final List<Mark> subjectMark = new LinkedList<>();
 
     public Subject getSubject() {
         return subject;
@@ -19,13 +19,16 @@ public class AverageMarkSubject {
         return subjectMark;
     }
 
-    public void setSubjectMark(List<Mark> subjectMark) {
-        this.subjectMark = subjectMark;
-    }
 
     public double averageMark(){
         double sumMark = 0;
         for (Mark mark : subjectMark) sumMark += mark.getCurrentMark();
         return sumMark/subjectMark.size();
+    }
+
+
+    @Override
+    public int compareTo(AverageMarkSubject o) {
+        return subject.compareTo(o.getSubject());
     }
 }
