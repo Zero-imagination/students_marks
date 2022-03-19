e9a7ff9c30db8556bf4f6216c8ffc092b21f5a88
Zero-imagination <disagreed7@gmail.com>
2022-03-19 13:53:13 +0300

    small string permutations

--- src/main/java/gui/boxes/BoxSearchBar.java
+++ src/main/java/gui/boxes/BoxSearchBar.java
@@ -8,21 +8,21 @@ import java.awt.*;
 
 public class BoxSearchBar {
     public static Box create(){
-        Box boxSearchBar = new Box(BoxLayout.X_AXIS);
         JLabel labelSearchBar = new JLabel("Введите данные студента");
-        JTextField searchBar = new JTextField();
-        JButton btnUpdate = new JButton("Поиск");
+        labelSearchBar.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
+        JTextField searchBar = new JTextField();
+        searchBar.setMaximumSize(new Dimension(200,32));
+        searchBar.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        JButton btnUpdate = new JButton("Поиск");
+        btnUpdate.addActionListener(e -> TableStudents.getTable().setModel(TableModelStudents.create(searchBar.getText())));
+
+        Box boxSearchBar = new Box(BoxLayout.X_AXIS);
+        boxSearchBar.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         boxSearchBar.add(labelSearchBar);
         boxSearchBar.add(searchBar);
         boxSearchBar.add(btnUpdate);
-
-        btnUpdate.addActionListener(e -> TableStudents.getTable().setModel(TableModelStudents.create(searchBar.getText())));
-
-        boxSearchBar.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        labelSearchBar.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        searchBar.setMaximumSize(new Dimension(200,32));
-        searchBar.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         return boxSearchBar;
     }
 }
