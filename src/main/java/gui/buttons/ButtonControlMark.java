3eb8bbba9ecd6a0279ac17cca2f9ed0d876c5e29
Zero-imagination <disagreed7@gmail.com>
2022-03-19 13:59:52 +0300

    moved the interaction with students and marks to a separate evaluation package

--- src/main/java/gui/buttons/ButtonControlMark.java
+++ src/main/java/gui/buttons/ButtonControlMark.java
@@ -1,46 +1,39 @@
-package gui;
+package gui.buttons;
 
-import gui.boxes.BoxStudent;
+import gui.Gui;
+import gui.boxes.BoxMarkInfo;
+import gui.boxes.BoxStudentInfo;
 import gui.tables.TableAvgMarks;
 import gui.tables.TableStudents;
 import gui.tables.TableSubjectMarks;
 import gui.tables.model.TableModelAvgMarks;
 import gui.tables.model.TableModelSubjectMarks;
-import models.Mark;
-import models.Student;
-import services.MarkServiceImpl;
-import services.StudentServiceImpl;
-import services.SubjectServiceImpl;
+import evaluation.model.Mark;
+import evaluation.model.Student;
+import evaluation.services.MarkServiceImpl;
+import evaluation.services.StudentServiceImpl;
+import evaluation.services.SubjectServiceImpl;
 import javax.swing.*;
 import java.util.Objects;
 
 
 public class ButtonControlMark {
     private static JButton buttonUpdate;
-
     private static JButton buttonDelete;
     private static JButton buttonCreate;
 
-    public static void setButtonUpdate(JButton buttonUpdate) {
-        ButtonControlMark.buttonUpdate = buttonUpdate;
-    }
-
-    public static void setButtonDelete(JButton buttonDelete) {
-        ButtonControlMark.buttonDelete = buttonDelete;
-    }
-
-    public static void setButtonCreate(JButton buttonCreate) {
+    public static void setButtonControlMark(JButton buttonCreate, JButton buttonUpdate, JButton buttonDelete){
         ButtonControlMark.buttonCreate = buttonCreate;
+        ButtonControlMark.buttonUpdate = buttonUpdate;
+        ButtonControlMark.buttonDelete = buttonDelete;
     }
 
     public static void enableButtonCreateMark(boolean enable){
         buttonCreate.setEnabled(enable);
     }
-
     public static void enableButtonUpdateDeleteMark(boolean enable){
         buttonUpdate.setEnabled(enable);
         buttonDelete.setEnabled(enable);
-        //buttonCreate.setEnabled(enable);
     }
 
     public static JButton create(TypeButton typeButton){
@@ -49,17 +42,17 @@ public class ButtonControlMark {
         button.addActionListener(e -> {
             String messageWhatChange = "";
             try {
-                if (BoxStudent.getInfoTextFields().get(1).getText() == null || BoxStudent.getInfoTextFields().get(1).getText().length() == 0) {
+                if (BoxStudentInfo.getInfoTextFields().get(1).getText() == null || BoxStudentInfo.getInfoTextFields().get(1).getText().length() == 0) {
                     throw new IllegalArgumentException();
                 }
-                String selectedSubject = BoxStudent.getSelectedSubjectName().getText();
+                String selectedSubject = BoxStudentInfo.getSelectedSubjectName().getText();
                 Mark mark = new Mark();
                 int selectedStudentId = TableStudents.getSelectedStudent().getId();
                 //данный if нужен для того чтобы создавать поля у новой оценки без выделения уже существующих
 
-                if ((BoxStudent.getInfoMarkFields().get(0)).isEnabled()){
+                if ((BoxMarkInfo.getInfoMarkFields().get(0)).isEnabled()){
                     SubjectServiceImpl subjectService = new SubjectServiceImpl();
-                    mark.setSubject(subjectService.readSubject(Objects.requireNonNull(((JComboBox<?>) BoxStudent.getInfoMarkFields().get(0)).getSelectedItem()).toString()));
+                    mark.setSubject(subjectService.readSubject(Objects.requireNonNull(((JComboBox<?>) BoxMarkInfo.getInfoMarkFields().get(0)).getSelectedItem()).toString()));
                 }else {
                     if (TableSubjectMarks.getSelectedMark()!=null){
                         mark.setSubject(TableSubjectMarks.getSelectedMark().getSubject());
@@ -70,7 +63,7 @@ public class ButtonControlMark {
                 }
 
                 mark.setStudentId(TableStudents.getSelectedStudent().getId());
-                mark.setCurrentMark(Double.parseDouble(((JTextField) BoxStudent.getInfoMarkFields().get(1)).getText()));
+                mark.setCurrentMark(Double.parseDouble(((JTextField) BoxMarkInfo.getInfoMarkFields().get(1)).getText()));
                 switch (typeButton){
                     case CREATE -> {
                         messageWhatChange = "Оценка "+mark+" добавлена студенту "+TableStudents.getSelectedStudent().onlyFullName();
@@ -98,8 +91,6 @@ public class ButtonControlMark {
                 TableSubjectMarks.getTable().setModel(TableModelSubjectMarks.create());
 
                 JOptionPane.showMessageDialog(Gui.getMainFrame(), "Данные успешно отредактированы! \n"+messageWhatChange);
-
-
             }catch (IllegalArgumentException ex) {
                 JOptionPane.showMessageDialog(Gui.getMainFrame(), "Введены не корректные данные!");
             }
